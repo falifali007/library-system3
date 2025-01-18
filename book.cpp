daaa643eda042b63b985d7f788c1264ddf7cd7c4
falifali007 <faridaabu3@gmail.com>
2025-01-18 18:41:08 +0100

    Update book.cpp

--- book.cpp
+++ book.cpp
@@ -1,15 +1,14 @@
 #include "book.h"
 
-// Constructor
 Book::Book(const std::string& title, const std::string& author, const std::string& isbn)
     : title(title), author(author), isbn(isbn) {}
 
-// Getter for title (const)
+
 std::string Book::getTitle() const {
     return title;
 }
 
-// Setter for title
+
 void Book::setTitle(const std::string& newTitle) {
     title = newTitle;
 }
